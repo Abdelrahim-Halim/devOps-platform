b4665a927eeb3b2b6fa73d35ce2f3798b0dd1254
halim <abdohalem91@gmail.com>
2020-08-13 06:04:12 +0200

    Create an cluster using eks module

--- terraform-eks-cluster/platform.tfvars
+++ terraform-eks-cluster/platform.tfvars
@@ -1,1 +1,5 @@
 region = "us-east-2"
+
+
+worker_instance_type = "t2.small"
+master_instance_type = "t2.medium"
--- terraform-eks-cluster/variables.tf
+++ terraform-eks-cluster/variables.tf
@@ -4,3 +4,14 @@ variable "region" {
     description = "AWS region"
 }
 
+variable "worker_instance_type" {
+    typy = string
+    default = "t2.small"
+    description = "typy of the worker node in the cluster"
+}
+
+variable "master_instance_type" {
+    typy = string
+    default = "t2.medium"
+    description = "typy of the worker node in the cluster"
+}
