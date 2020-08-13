a60cf5c228ed242b191ea7374ca5d990c1dd3ec7
halim <abdohalem91@gmail.com>
2020-08-13 06:20:16 +0200

    update variables file

--- terraform-eks-cluster/variables.tf
+++ terraform-eks-cluster/variables.tf
@@ -1,17 +1,17 @@
 variable "region" {
-    typy        = string
+    type        = string
     default     = "us-east-2"
     description = "AWS region"
 }
 
 variable "worker_instance_type" {
-    typy = string
+    type = string
     default = "t2.small"
     description = "typy of the worker node in the cluster"
 }
 
 variable "master_instance_type" {
-    typy = string
+    type = string
     default = "t2.medium"
     description = "typy of the worker node in the cluster"
 }
